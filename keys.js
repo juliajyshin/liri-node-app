1a9012458e6e987c920bb281edb651d33f130163
Julia Shin <juliashin@Julias-iMac.attlocal.net>
2017-12-11 23:47:50 -0800

    package.json

---
+++ keys.js
@@ -0,0 +1,10 @@
+JavaScript
+console.log('this is loaded');
+var twitterKeys = {
+    consumer_key: 'HAuuUm059D4iYgA72j5tnRR2E'>,
+    consumer_secret: 'K6oyd5VYoIOe9YUrqKTiyStV8mFW88xp6H9tcdTWkXlUa6c5zf'>,
+    access_token_key: '940476247703830528-tMZHcoOJMRmn4WBWzrsBiK8OR5prN9q'>,
+    access_token_secret: <'DnZ14o5mrS2cq9bhQo9BCqzV5KFV8n5r40hsZAfcYPm37'>,
+}
+
+module.expoerts = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,7 @@
+// 7. At the top of the 'liri.js'file, write the code you need to grab from keys.js. Then store the keys ina variable.
+
+// 8. Make it so liri.js can take in one of the following commands:
+// *`my-tweets`
+// *`spotify-this-song`
+// *`movie-this`
+// *`do-what-it-says`
